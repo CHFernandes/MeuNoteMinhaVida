fab2eec3485f6358c61a9217f6448de785c83bfd
CHFernandes <chfernandes01@hotmail.com>
2019-05-10 08:15:21 -0300

    alterado vequipemp

--- mnmv/js/vequipemp.js
+++ mnmv/js/vequipemp.js
@@ -2,15 +2,17 @@ $(document).ready(function () {
     $.ajax({
         type: "GET",
         datatype: "json",
-        url: "../php/vequipemp.php",
+        url: "../php/vequip.php",
         success: function (lista) {
-            
+
             var stringster = JSON.parse(lista);
 
             $("#lista").html("");
             for (var i = stringster.length - 1; i >= 0; i--) {
-                $("#lista").append(
-                    '<tr><td>' + stringster[i].numeracao + '</td><td>'+ stringster[i].tipo +'</td></tr>');
+                if (stringster[i].status == 1) {
+                    $("#lista").append(
+                        '<tr><td>' + stringster[i].numeracao + '</td><td>' + stringster[i].tipo + '</td></tr>');
+                }
             }
         }
     });
