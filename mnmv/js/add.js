7d7299be6281f092caa39eb37540352bd4016e8e
CHFernandes <chfernandes01@hotmail.com>
2019-05-13 09:51:05 -0300

    finalizado insert_0

--- mnmv/js/add.js
+++ mnmv/js/add.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
+    var i = 0;
     $('#add').click(function(){
+        i = i +1;
         $('#body').append(
-        '<tr><td>Tipo</td><td><select class="tipo"><option></option><option value="N">Notebook</option><option value="F">Fonte</option><option value="T">Torre</option><option value="C">Carrinho</option></select></td></tr><tr><td>Equipamento</td><td><input type="text" class="equip"></td></tr>'
+        '<tr><td>Tipo</td><td><select class="tipo" name = "'+ i +'"><option></option><option value="N">Notebook</option><option value="F">Fonte</option><option value="T">Torre</option><option value="C">Carrinho</option></select></td></tr><tr><td>Equipamento</td><td><select class="equip" name="'+ i +'"></select></td></tr>'
         );
     });
 });
--- mnmv/js/insert_0.js
+++ mnmv/js/insert_0.js
@@ -1,21 +1,41 @@
 $(document).ready(function () {
+    $(body).on('change', '.tipo', function () {
+        alert("Mudou");
+        var tipo = new Array();
+        var name = new Array();
+        
+        $('.tipo').each(function () {
+            name.push($(this).attr('name'));
+        });
+            
+        $('.tipo').each(function(){
+            tipo.push($(this).val());
+        });
 
-    $.ajax({
-        type: "GET",
-        datatype: "json",
-        url: "../php/insert_0.php",
-        success: function (lista) {
-
-            var stringster = JSON.parse(lista);
-
-            $(".equip").html("");
-            for (var i = stringster.length - 1; i >= 0; i--) {
+        $.ajax({
+            type: "GET",
+            datatype: "json",
+            url: "../php/insert_0.php",
+            success: function (lista) {
                 
-                    $("#inserir").append(
-                        "<tr><td>" + stringster[i].numeracao + "</td><td>" + stringster[i].tipo + "</td></tr>");
-                
+                alert("Success");
+                var stringster = JSON.parse(lista);
+
+                $(".equip").html("");
+                for (var i = stringster.length - 1; i >= 0; i--) {
+                    var o = 0;
+                    alert("for");
+                    while(o < name.length){
+                        alert(name[o]);
+                        alert(tipo[o]);
+                        if (stringster[i].tipo == tipo[o]){
+                            $(".equip[name = " + name[o] + "]").append(
+                            "<option>" + stringster[i].numeracao + "</option>");
+                        }
+                        o = o + 1;
+                    }    
+                }
             }
-        }
+        });
     });
-
 });
