83662f818347ccb2616be8614f6d3e6671465d60
Thiago Jose Fagundes Saquette <43481302+thigosaquette@users.noreply.github.com>
2019-05-15 09:27:03 -0300

    Add files via upload

---
+++ baixa.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $("#enviar").click(function () {
+        $.ajax({
+            type: "POST",
+            url: "../php/baixa.php",
+           data: {
+                num: $(".equip").val(),
+                tipo: $(".tipo").val()
+            },
+            success: function (retorno) {
+                
+                if (retorno == '1') {
+                    alert("Baixa realizada com sucesso");
+                    window.location = 'niaa.html';
+                } else {
+                    alert("Erro");
+                }
+            }
+        });
+    });
+});
