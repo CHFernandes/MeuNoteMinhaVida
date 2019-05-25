df618bdab5b5f566e0ba012a520b9976273df20b
CHFernandes <chfernandes01@hotmail.com>
2019-05-24 21:33:00 -0300

    emprestimo travado

--- mnmv/php/ap.php
+++ mnmv/php/ap.php
@@ -4,7 +4,11 @@ session_start();
 if($_SESSION['tipo'] == '2'){
     echo "1";
 }else {
-    echo "0";
+    if($_SESSION['tipo'] == '1'){
+       echo "2"; 
+    }else{
+      echo "0";  
+    }
 }
 
 ?>
