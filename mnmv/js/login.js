1bcb54615258e5bd8afa35c8594c6a88668b98c5
Thiago Jose Fagundes Saquette <43481302+thigosaquette@users.noreply.github.com>
2019-06-11 14:53:50 -0300

    Add files via upload

--- mnmv/js/login.js
+++ mnmv/js/login.js
@@ -23,3 +23,28 @@ $(document).ready(function () {
         });
     });
 });
+document.addEventListener('keypress', function(e){
+  if(e.which == 13){
+    $.ajax({
+            type: "POST",
+            url: "php/login.php",
+            data: {
+                matricula: $("#matricula").val(),
+                senha: $("#senha").val()
+            },
+            success: function (retorno) {
+                if (retorno == '2') {
+                    window.location = 'html/niaa.html';
+                } else {
+                    if (retorno == '1'){
+                        window.location = 'html/menu.html';
+                    }else if(retorno == '3'){
+                        alert("Usuário bloqueado por tempo indeterminado, favor entrar em contato com o NIAA");
+                    }else{
+                        alert("Usuário ou senha incorretos");
+                    }
+                }
+            }
+        });
+  }
+}, false);
